236b66b4c6af365e501522c8fe286ca92dfa36dc
Andrew Ivanov <a.ivanov@online-express.ru>
2020-05-07 20:52:10 +0300

    edit

--- script.js
+++ script.js
@@ -12,6 +12,74 @@ const list = document.querySelector('.list');
 const formButton = document.querySelector('.form__submit');
 const formInput = document.querySelector('.form__input');
 
+const state = {
+    mode: 'add',
+    index: null
+}
+
+formButton.addEventListener('click', addFunc);
+render();
+
+function render() {
+    clean();
+    todos.forEach(renderElement);
+    setHandlers();
+}
+
+function reset() {
+    state.mode = 'add';
+    state.index = null;
+}
+
+function clean() {
+    formButton.value = 'Добавить';
+    list.innerHTML = '';
+    reset();
+}
+
+function setHandlers() {
+    list.querySelectorAll('.delete').forEach((button) => {
+        button.addEventListener('click', deleteItem);
+    });
+
+    list.querySelectorAll('.duplicate').forEach((button) => {
+        button.addEventListener('click', duplicateItem)
+    });
+
+    list.querySelectorAll('.edit').forEach((button) => {
+        button.addEventListener('click', editItem)
+    });
+}
+
+function renderElement(text, index) {
+    const element = itemTemplate.cloneNode(true);
+
+    element.querySelector('.item__text').innerText = text;
+    element.querySelector('.list__item').setAttribute('id', index);
+    list.appendChild(element);
+}
+
+function addFunc() {
+    const text = formInput.value;
+    if (state.mode === 'edit') {
+        todos[state.index] = text;
+    }
+    if (state.mode === 'add') {
+        todos.unshift(text);
+    }
+    formInput.value = '';
+    render();
+}
+
+function editItem(e) {
+    const index = e.target.parentNode.getAttribute('id');
+
+    formInput.value = todos[index];
+    formButton.value = 'Сохранить';
+    state.mode = 'edit';
+    state.index = index;
+}
+
 function deleteItem(e) {
     const index = e.target.parentNode.getAttribute('id');
     todos.splice(index, 1);
@@ -26,37 +94,3 @@ function duplicateItem(e) {
     render();
 }
 
-function render() {
-    list.innerHTML = '';
-    todos.forEach(renderElement);
-    setHandlers();
-}
-
-function setHandlers() {
-    list.querySelectorAll('.delete').forEach((button) => {
-        button.addEventListener('click', deleteItem);
-    })
-
-    list.querySelectorAll('.duplicate').forEach((button) => {
-        button.addEventListener('click', duplicateItem)
-    })
-}
-
-function renderElement(text, index) {
-    const element = itemTemplate.cloneNode(true);
-
-    element.querySelector('.item__text').innerText = text;
-    element.querySelector('.list__item').setAttribute('id', index);
-    list.appendChild(element);
-}
-
-formButton.addEventListener('click', addFunc);
-
-function addFunc() {
-    const text = formInput.value;
-    todos.unshift(text);
-    formInput.value = '';
-    render();
-}
-
-render();
